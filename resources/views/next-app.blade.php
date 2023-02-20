3d07a75a936824d3df67188468c2378bcb8cf956
Mariano <marianotorresleyva@gmail.com>
2023-02-20 17:48:16 -0500

    Inicio de proyecto

---
+++ resources/views/next-app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Next.js App</title>
+</head>
+
+<body>
+    <div id="__next"></div>
+    <script src="{{ asset('next-app/.next/static/runtime/webpack.js') }}"></script>
+    <script src="{{ asset('next-app/.next/static/chunks/main.js') }}"></script>
+</body>
+
+</html>
